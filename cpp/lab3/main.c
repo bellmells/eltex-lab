09cbf69b59b1049cdfc162d0cb38138c3bb69400
bellmells <bellamahtieva@gmail.com>
2018-12-01 18:59:19 +0700

    lab3 static

---
+++ cpp/lab3/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct worker
+{
+	char name[64];
+	int year_birth;
+	int department_number;
+	int salary;
+};
+
+static int cmp_func(const void *p1, const void *p2)
+{
+	struct worker * w1 = (struct worker *) p1;
+	struct worker * w2 = (struct worker *) p2;
+	
+	return w1->year_birth-w2->year_birth;
+}
+
+#define LEN 5
+
+int main(int argc, char const *argv[])
+{
+	struct worker workers[LEN];
+	for (int i = 0; i < LEN; ++i)
+	{
+		printf("Введите фамилию\n");
+		scanf("%s", workers[i].name);
+		printf("Введите год рождения\n");
+		scanf("%d", &workers[i].year_birth);
+		printf("Введите номер отдела\n");
+		scanf("%d", &workers[i].department_number);
+		printf("Введите сумму оклада\n");
+		scanf("%d", &workers[i].salary);
+
+	}
+
+	qsort(workers, LEN, sizeof(struct worker), cmp_func);
+
+	printf("Cортировка ...\n");
+
+	for (int i = 0; i < LEN; ++i)
+	{
+		printf("Введите фамилию\n");
+		printf("%s\n", workers[i].name);
+		printf("Введите год рождения\n");
+		printf("%d\n", workers[i].year_birth);
+		printf("Введите номер отдела\n");
+		printf("%d\n", workers[i].department_number);
+		printf("Введите сумму оклада\n");
+		printf("%d\n", workers[i].salary);
+	}
+
+	return 0;
+}
